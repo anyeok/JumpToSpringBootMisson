4235367e748564dc5389d71669838eba6c9d8eb9
anyeok <https://github.com/anyeok>
2024-09-24 10:14:42 +0900

    1

---
+++ src/main/java/com/example/SpringBootMisson1/SpringBootMisson1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootMisson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMisson1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMisson1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootMisson1/SpringBootMisson1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootMisson1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMisson1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
